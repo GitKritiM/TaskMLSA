46b25581e6b27bb2794b65cb9700af3c0c4010f5
GitKritiM <154223581+GitKritiM@users.noreply.github.com>
2023-12-21 23:02:46 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/3.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Networking ";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "web devlopment",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Frontend and backend",
+    url: "https://youtu.be/PRSyHTajxPk?si=DHqC4cqes6AHsc58",
   },
   {
-    title: "Web Development for Beginners",
+    title: "About Html and CSS",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Designing",
+    url: "https://www.youtube.com/live/WYo5bK7xxK0?si=YJrXKIZljdAhEsEr",
   },
   {
-    title: "My Resume Site",
+    title: "Code",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Coding Languages used for Website Design ",
+    url: "https://youtu.be/j9wWPEmxSZg?si=G-TknqZrWN3x_-65",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "About GitHub",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Profile",
+    url: "https://github.com/GitKritiM",
   },
 ];
 
